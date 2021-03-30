53ec5fc58e368edfddd74fce4643ebe9e1bc2e84
Alan Roddick <aroddick16@gmail.com>
2021-03-30 00:13:03 -0700

    Initial Commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      temp
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      temp
+    </div>
+  )
+}
+
+export default Home;
