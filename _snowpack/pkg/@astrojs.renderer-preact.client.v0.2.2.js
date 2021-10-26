060baa05e464f4e58ce4b07a6ca6e01b866f364b
Alan Roddick <aroddick16@gmail.com>
2021-10-25 23:42:56 -0700

    Migrate to astro

---
+++ _snowpack/pkg/@astrojs.renderer-preact.client.v0.2.2.js
@@ -0,0 +1,1 @@
+import{v as e,S as t}from"../../preact.v10.5.15-340f76cd.js";const a=({value:t})=>t?e("astro-fragment",{dangerouslySetInnerHTML:{__html:t}}):null;a.shouldComponentUpdate=()=>!1;var o=o=>(r,l,n)=>t(e(r,l,e(a,{value:n})),o);export{o as default};
