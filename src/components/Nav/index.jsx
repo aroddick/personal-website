80f35826742d0bd2fbe789a3b04d1ebd16359254
Alan Roddick <aroddick@ucsb.edu>
2022-02-06 21:52:57 -0800

    Fix mobile nav stuck opacity and collapse navbar on click

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -12,11 +12,11 @@ function Nav() {
   return (
     <nav id="header" className={Styles.container}>
       <div className={visible ?  Styles.links + " " + Styles.responsive : Styles.links}>
-        <a href='#home'>Home</a>
-        <a href='#about'>About</a>
-        <a href='#resume'>Resume</a>
-        <a href='#projects'>Projects</a>
-        <a href='#contact'>Contact</a>
+        <a href='#home' onClick={() => toggleLinks()}>Home</a>
+        <a href='#about' onClick={() => toggleLinks()}>About</a>
+        <a href='#resume' onClick={() => toggleLinks()}>Resume</a>
+        <a href='#projects' onClick={() => toggleLinks()}>Projects</a>
+        <a href='#contact' onClick={() => toggleLinks()}>Contact</a>
         <button className={Styles.icon} onClick={() => toggleLinks()}>
           <FontAwesomeIcon icon={faBars} />
         </button>
